cf7e7094e166e6b531b79aa57ef001649015d705
blongden73 <ben@cord.com>
2021-07-16 00:41:28 +0100

    added header interaction

--- assets/main.js
+++ assets/main.js
@@ -1,22 +1,17 @@
 console.log('hola ani');
 
 function mouse(){
-  document.addEventListener('mousemove', function(){
+  document.addEventListener('mousemove', _.debounce(function(){
     var imageContainer = document.querySelector('.header-image-container');
     var images = imageContainer.querySelectorAll('img');
     console.log(images);
     var random = Math.floor(Math.random() * images.length);
     console.log('im moving', random);
     for(i=0; i<images.length; i++){
-      // setTimeout(function(){
-      // }, 500, images)
       images[i].classList.remove('visible');
-
-    }
-    setTimeout(function(){
       images[random].classList.add('visible');
-    }, 1000)
-  })
+    }
+ },50));
 }
 
 function init(){
